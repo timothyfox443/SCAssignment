86c191923b26c6b5ee0c67dd178e49ea8100790a
Timothy Fox <fox5681@students.cptc.edu>
2021-07-11 15:42:48 -0700

    I made this!

---
+++ AlmostEmpty3-1StarterCode/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlmostEmpty3_1StarterCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home";
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About";
+            return View();
+        }
+        public IActionResult Content()
+        {
+            ViewData["Title"] = "Content";
+            return View();
+        }
+
+
+    }
+}
